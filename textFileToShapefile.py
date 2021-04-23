33e06e5257d492ec00e3768924f2e77385f26bc1
TahsinGormus <50828412+TahsinGormus@users.noreply.github.com>
2021-04-23 06:24:50 +0300

    Update textFileToShapefile.py

--- textFileToShapefile.py
+++ textFileToShapefile.py
@@ -1,5 +1,6 @@
 """
 Create shapefile from a textfile
+The longitude and latitude columns should be present in the text file named as LON and LAT.
 """
 import pandas as pd
 import geopandas as gpd
@@ -12,10 +13,8 @@ df = pd.read_csv('data.txt', delimiter = "\t")
 gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.LON, df.LAT))
 
 # Set the GeoDataFrame's coordinate system to WGS84 (i.e. epsg code 4326)
-
 gdf.crs = from_epsg(4326)
 
 # Writing the shapefile
 outfp = "data.shp"
 gdf.to_file(outfp)
-
